3365529ef202d62841cb6b582286129056b8cfb2
Sahan Fernando <sahan.h.fernando@gmail.com>
2018-01-28 15:12:57 +1100

    Got rid of static variables

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::ffi::OsString;
 use std::io::{Error, ErrorKind, Read, Write};
 use std::path::PathBuf;
 use std::process::exit;
+use std::collections::HashMap;
 
 use argparse::{ArgumentParser, StoreTrue, StoreOption};
 
@@ -30,21 +31,45 @@ fn write_file(file_name: &PathBuf, data: &[u8]) -> Result<usize, Error> {
     try!(File::create(file_name.as_os_str())).write(data)
 }
 
-static mut _AVAILABLE_BACKLIGHTS: Option<Vec<OsString>> = None;
-
-fn initialize_ab() {
-    unsafe {
-        _AVAILABLE_BACKLIGHTS = Some(read_dir("/sys/class/backlight")
-            .expect("Could not open /sys/class/backlights")
-            .filter(|x| x.is_ok())
-            .map(|x| x.unwrap().file_name())
-            .collect());
-    }
+struct Backlights {
+    by_name: HashMap<OsString, Backlight>,
 }
 
-fn available_backlights() -> Option<Vec<OsString>> {
-    unsafe {
-        _AVAILABLE_BACKLIGHTS.clone()
+impl Backlights {
+    pub fn new() -> Self {
+        let mut hm = HashMap::new();
+        for name in read_dir("/sys/class/backlight").expect("Could not open /sys/class/backlights") {
+            match name {
+                Ok(v) => {
+                    let file_name = v.file_name();
+                    let bl = Backlight::from_name(&file_name).expect("Failed to open backlight");
+                    hm.insert(file_name, bl);
+                }
+                Err(e) => {
+                    panic!("{}", e);
+                },
+            }
+        }
+        Backlights {
+            by_name: hm,
+        }
+    }
+    pub fn print_backlights(&self){
+        println!("Printing device names:");
+        for name in self.by_name.keys() {
+            println!("{:?}", name);
+        }
+    }
+    pub fn default_backlight(mut self) -> Result<Backlight, Error> {
+        let os_default = OsString::from("default");
+        Ok(if self.by_name.contains_key(&os_default) {
+            self.by_name.remove(&os_default).unwrap()
+        } else if self.by_name.len() > 0 {
+            let key = self.by_name.keys().nth(0).unwrap().clone();
+            self.by_name.remove(&key).unwrap()
+        } else {
+            return Err(Error::new(ErrorKind::Other, "No backlights detected"))
+        })
     }
 }
 
@@ -56,34 +81,19 @@ struct Backlight {
 }
 
 impl Backlight {
-    fn default() -> Result<Self, Error> {
-        let ab = available_backlights().unwrap();
-        let name = if ab.contains(&OsString::from("default")) {
-            OsString::from("default")
-        } else if ab.len() > 0 {
-            ab[0].clone()
-        } else {
-            return Err(Error::new(ErrorKind::Other, "No backlights detected"))
-        };
-        Self::from_name(&name)
-    }
     fn from_name(file_name: &OsString) -> Result<Self, Error>  {
-        if available_backlights().unwrap().contains(file_name) {
-            let mut path_buf = PathBuf::from("/sys/class/backlight");
-            path_buf.push(file_name);
-            path_buf.push("brightness");
-            let brightness = try!(read_file_to_usize(&path_buf));
-            path_buf.pop();
-            path_buf.push("max_brightness");
-            let max_brightness = try!(read_file_to_usize(&path_buf));
-            Ok(Self {
-                name: file_name.clone(),
-                brightness: brightness,
-                max_brightness: max_brightness
-            })
-        } else {
-            Err(Error::new(ErrorKind::Other, "Backlight not found"))
-        }
+        let mut path_buf = PathBuf::from("/sys/class/backlight");
+        path_buf.push(file_name);
+        path_buf.push("brightness");
+        let brightness = try!(read_file_to_usize(&path_buf));
+        path_buf.pop();
+        path_buf.push("max_brightness");
+        let max_brightness = try!(read_file_to_usize(&path_buf));
+        Ok(Self {
+            name: file_name.clone(),
+            brightness: brightness,
+            max_brightness: max_brightness
+        })
     }
     fn set_brightness(&mut self, new_value: usize) {
         let mut path_buf = PathBuf::from("/sys/class/backlight");
@@ -94,15 +104,7 @@ impl Backlight {
     }
 }
 
-fn print_backlights(){
-    println!("Printing device names:");
-    for name in available_backlights().unwrap() {
-        println!("{:?}", name);
-    }
-}
-
 fn main() {
-    initialize_ab();
     let mut backlight_name: Option<String> = None;
     let mut desired_brightness: Option<usize> = None;
     let mut should_print_backlights = false;
@@ -115,7 +117,8 @@ fn main() {
         ap.parse_args_or_exit();
     }
     if should_print_backlights {
-        print_backlights();
+        let bls = Backlights::new();
+        bls.print_backlights();
         exit(0);
     } else {
         let mut backlight = match backlight_name.map(|x| OsString::from(x)) {
@@ -123,11 +126,15 @@ fn main() {
                 Ok(v) => v,
                 Err(e) => {
                     println!("Failed to open backlight: {:?}", e);
-                    print_backlights();
+                    let bls = Backlights::new();
+                    bls.print_backlights();
                     exit(1);
                 }
             },
-            None => Backlight::default().expect("Failed to open default backlight")
+            None => {
+                let bls = Backlights::new();
+                bls.default_backlight().expect("Failed to open default backlight")
+            }
         };
         match desired_brightness {
             Some(v) => {
